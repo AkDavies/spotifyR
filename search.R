873d13386b813931c68cc770ad19f3da18bd2dbf
Akindele Davies <aod@andrew.cmu.edu>
2017-12-27 18:58:43 -0500

    Verified acess token process and performed test search of a track

--- search.R
+++ search.R
@@ -1,1 +1,23 @@
 library(httr)
+library(glue)
+
+# Spotify Web API Base URL
+BASE_URL <- "https://api.spotify.com"
+
+SPOTIFY_CLIENT_ID <- "7a10a16f6b5649b8a3d4c837ab30b1d2"
+SPOTIFY_SECRET <- "a970c16c23c64b02a5a04af82bb571c4"
+
+SPOTIFY_AUTHORIZATION_URL <- "https://accounts.spotify.com/api/token"
+
+SPOTIFY_ACCESS_TOKEN <- POST(SPOTIFY_AUTHORIZATION_URL,
+              authenticate(SPOTIFY_CLIENT_ID,SPOTIFY_SECRET),
+              # add_headers(Authorization = glue("Basic ",SPOTIFY_CLIENT_ID,":",SPOTIFY_SECRET,.sep = "")),
+              body = list(grant_type = "client_credentials"),
+              encode = "form"
+              ) %>% content() %>% {.$access_token}
+
+test_url <- "https://api.spotify.com/v1/tracks/3n3Ppam7vgaVa1iaRUc9Lp"
+test_track <- GET(test_url,
+                  add_headers(Authorization = glue('Bearer {SPOTIFY_ACCESS_TOKEN}'))
+                  ) 
+
