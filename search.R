0bcf5c245862c5866f5fb8e8c4ca0c687beadf91
Akindele Davies <aod@andrew.cmu.edu>
2018-01-08 02:09:19 -0500

    Finished extract_artists and rough draft of search_albums. Modified search_tracks to include full list of artists by using extract_artists.

--- search.R
+++ search.R
@@ -25,11 +25,6 @@ request_access_token <- function(client_id, secret_key){
 
 SPOTIFY_ACCESS_TOKEN <- request_access_token(SPOTIFY_CLIENT_ID, SPOTIFY_SECRET)
 
-test_url <- "https://api.spotify.com/v1/tracks/3n3Ppam7vgaVa1iaRUc9Lp"
-test_track <- GET(test_url,
-                  add_headers(Authorization = glue('Bearer {SPOTIFY_ACCESS_TOKEN}'))
-                  ) 
-
 test_query <- "Bryce Vine"
 
 is_valid_search_string <- function(search_string){
@@ -55,15 +50,15 @@ search_tracks <- function(search_string, by = c("name", "artist", "album"), limi
                         )
     tracks_raw <- content(api_response)$tracks$items
     tracks <- map_df(tracks_raw, 
-           ~data_frame(name = .$name, 
-                       artist = .$artists[[1]]$name, 
-                       album = .$album$name, 
-                       `track no` = .$track_number, 
-                       popularity = .$popularity, 
-                       duration = .$duration_ms, 
-                       explicit = .$explicit, 
-                       uri = .$uri
-                       )
+                     ~data_frame(name = .$name, 
+                                 artist = extract_artists(.$artists), 
+                                 album = .$album$name, 
+                                 `track no` = .$track_number, 
+                                 popularity = .$popularity, 
+                                 duration = .$duration_ms, 
+                                 explicit = .$explicit, 
+                                 uri = .$uri
+                     )
            )
     tracks
 }
@@ -72,22 +67,49 @@ make_GET_request <- function(url, headers = NULL, query_params = NULL){
     call <- match.call()
     api_resposne <- do.call("GET", call[-1])
 }
+
+extract_artists <- function(artist_objects){
+    map_chr(artist_objects, function(x){x$name}) %>% paste0(collapse = ", ")
+}
+
 get_albums <- function(id, album_type = c("album", "single", "appears_on", "compilation"),
                        market = "US", limit = 20){
     #Validate inputs
     if(!is_valid_search_string(id)) stop("Invalid ID")
-    if(!is.character(market) | length(market) != 2) stop("Invalid market")
+    if(!is.character(market) | nchar(market) != 2) stop("Invalid market")
     if(!is_valid_limit(limit)) stop("Invalid Limit")
     album_type <- match.arg(album_type)
     
     api_response <- GET(modify_url(BASE_URL, path = glue('v1/artists/{id}/albums')),
-                    add_headers(Authorization = glue('Bearer {SPOTIFY_ACCESS_TOKEN}')),
-                    query = list(album_type = album_type,
-                                 market = market,
-                                 limit = limit
+                        add_headers(Authorization = glue('Bearer {SPOTIFY_ACCESS_TOKEN}')),
+                        query = list(album_type = album_type,
+                                     market = market,
+                                     limit = limit
                                  )
                     )
+                    
     
-    album_list <- content(api_response)$albums$items
+    albums_raw <- content(api_response)$items
+    album_ids <- map_df(albums_raw, ~data_frame(id = .$id))
+    album_ids <- paste0(album_ids$id, collapse = ",")
     
+    api_response <- GET(modify_url(BASE_URL, path = glue('v1/albums')),
+                        add_headers(Authorization = glue('Bearer {SPOTIFY_ACCESS_TOKEN}')),
+                        query = list(ids = album_ids)
+                        )
+    albums_raw <- content(api_response)$albums
+    albums <- map_df(albums_raw,
+                     ~data_frame(id = .$id,
+                                 name = .$name,
+                                 artist = extract_artists(.$artists),
+                                 album_type = .$album_type,
+                                 # genres = .$genres,
+                                 # Should convert release_data to date_time object using lubridate
+                                 release_date = .$release_date,
+                                 popularity = .$popularity
+                                 )
+                     )
+    albums
 }
+
+test_album_id <-  "5PxCTrv3Y1xVACfngpt7D2"
